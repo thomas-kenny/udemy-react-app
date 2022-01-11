17602ea88183130bf8228f30690e7b56a0b5b1e3
thomas-kenny <thomasarthurkenny@gmail.com>
2022-01-11 10:11:37 +0000

    arrow function preferred

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,7 +2,10 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log('HELLO')
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +13,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -2,7 +2,7 @@ import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "./Card"
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
